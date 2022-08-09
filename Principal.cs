462a544ebcb0e09b2bacb993903b4f48c4618c2b
gustavocern <gustavoargueta363@gmail.com>
2022-08-09 09:48:11 -0500

    Agregar archivos de proyecto.

---
+++ Principal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana3_ejemplo_de_menu_en_consola
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
