e5fe6a4ab1d5c1313c84ad8acae7d2fcad4513ec
gustavocern <gustavoargueta363@gmail.com>
2022-08-09 11:01:37 -0500

    agrege el codigo de menu

--- Menu.cs
+++ Menu.cs
@@ -8,5 +8,39 @@ namespace semana3_ejemplo_de_menu_en_consola
 {
     class Menu
     {
+        static void Main(string[] args)
+        {
+            string opcion;
+        int opcionint;
+
+        System.Console.WriteLine("Menu Principal de la Practica 1: ");
+            System.Console.WriteLine("Selecciona tu opcion: ");
+            opcion = Console.ReadLine();
+            opcionint = Int32.Parse(opcion);
+
+
+            switch (opcionint)
+            { 
+                case 1:
+                System.Console.WriteLine("Aqui llamo el programa1");
+                break;
+
+                case 2:
+                System.Console.WriteLine("Aqui llamo el programa2");
+                break;
+
+                case 3:
+                System.Console.WriteLine("Aqui llamo el programa3");
+                break;
+
+                case 4:
+                System.Console.WriteLine("Opcion no disponible");
+                break;
+
+            }
+
+
+    System.Console.ReadKey();
+        }
     }
 }
