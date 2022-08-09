216a3a7ca0ab57e69786f451bcabc75d88ae0fb4
gustavocern <gustavoargueta363@gmail.com>
2022-08-09 10:22:48 -0500

    Agrege los static void

--- Principal.cs
+++ Principal.cs
@@ -11,6 +11,30 @@ namespace semana3_ejemplo_de_menu_en_consola
         static void Main(string[] args)
         {
         }
+        static void menu_interno()
+        {
 
+        }
+
+        static void programa_if()
+        {
+
+        }
+        static void programa_for()
+        {
+
+        }
+        static void programa_swithc()
+        {
+
+        }
+        static void programa_while()
+        {
+
+        }
+        static void proframa_dowhile()
+        {
+
+        }
     }
 }
