50f54f141e60ce6313c3bc06652e118781beaef5
gustavocern <gustavoargueta363@gmail.com>
2022-08-09 10:41:37 -0500

    agregando codigos.

--- Ejemplo-if.cs
+++ Ejemplo-if.cs
@@ -8,5 +8,56 @@ namespace semana3_ejemplo_de_menu_en_consola
 {
     class Ejemplo_if
     {
+        public static void Main()
+        {
+            string myInput;
+            int myInt;
+            Console.Write("Dame un número: ");
+
+            myInput = Console.ReadLine();
+            myInt = Int32.Parse(myInput);
+
+            // comparación para una toma de decisión SIMPLE 
+            if (myInt > 0)
+            {
+                Console.WriteLine("el número {0} es mayor que cero", myInt);
+            }
+            // comparación para una toma de decisión SIMPLE 
+            if (myInt < 0)
+            {
+                Console.WriteLine("el número {0} es menor que cero", myInt);
+            }
+            // comparación para tomar UNA DE DOS decisiones 
+            if (myInt != 0)
+            {
+                Console.WriteLine("el número {0} es distinto de cero", myInt);
+            }
+            else
+            {
+                Console.WriteLine("el número {0} es igual a cero", myInt);
+            }
+
+            // decisión MÚLTIPLE 
+            if (myInt < 0 || myInt == 0)
+            {
+                Console.WriteLine("el número {0} es menor o igual a cero", myInt);
+            }
+            else if (myInt > 0 && myInt <= 10)
+            {
+                Console.WriteLine("el número {0} está entre 1 y 10", myInt);
+            }
+            else if (myInt > 10 && myInt <= 20)
+            {
+                Console.WriteLine("el número {0} está entre 11 y 20", myInt);
+            }
+            else if (myInt > 20 && myInt <= 30)
+            {
+                Console.WriteLine("el número {0} está entre 21 y 30", myInt);
+            }
+            else
+            {
+                Console.WriteLine("el número {0} es mayor que 30", myInt);
+            }
+        }
     }
 }
--- Ejemplo_swithc.cs
+++ Ejemplo_swithc.cs
@@ -8,5 +8,27 @@ namespace semana3_ejemplo_de_menu_en_consola
 {
     class Ejemplo_swithc
     {
+        public static void Main()
+        {
+            Console.WriteLine("ingrese un numero entre 1 y 3");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("valor es 1");
+                    break;
+                case 2:
+                    Console.WriteLine("valor es 2");
+                    break;
+                case 3:
+                    Console.WriteLine("valor es 3");
+                    break;
+                default:
+                    Console.WriteLine("no se encontro el valor");
+                    break;
+            }
+            Console.ReadKey();
+        }
     }
-}
+} 
+    
